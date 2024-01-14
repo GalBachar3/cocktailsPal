6c014f905c51bfad02ebc95f65c2d44d3eaf0305
itay <itay.jacobson@gmail.com>
2024-01-15 00:43:02 +0200

    login and register functionallity

--- cocktailsPal/app/src/main/java/com/example/cocktailspal/MainActivity.kt
+++ cocktailsPal/app/src/main/java/com/example/cocktailspal/MainActivity.kt
@@ -8,25 +8,29 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cocktailspal.model.user.User
+import com.example.cocktailspal.model.user.UserModel
 
 class MainActivity : AppCompatActivity() {
     var createNewAccount: TextView? = null
     var inputEmail: EditText? = null
     var inputPassword: EditText? = null
     var btnLogin: Button? = null
+    var emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
     var progressDialog: ProgressDialog? = null
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createNewAccount = findViewById(R.id.createNewAccount)
+        createNewAccount = findViewById<TextView>(R.id.createNewAccount)
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        inputEmail = findViewById(R.id.inputEmail)
-        inputPassword = findViewById(R.id.inputPassword)
-        btnLogin = findViewById(R.id.btnLogin)
+        inputEmail = findViewById<EditText>(R.id.inputEmail)
+        inputPassword = findViewById<EditText>(R.id.inputPassword)
+        btnLogin = findViewById<Button>(R.id.btnLogin)
         progressDialog = ProgressDialog(this)
         createNewAccount!!.setOnClickListener { view: View? ->
             startActivity(
@@ -36,5 +40,44 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+        btnLogin!!.setOnClickListener { view: View? -> PerformLogin() }
+    }
+
+    private fun PerformLogin() {
+        val email: String = inputEmail?.text.toString()
+        val password: String = inputPassword?.text.toString()
+        if (!email.matches(emailPattern.toRegex())) {
+            inputEmail?.error = "Enter correct email"
+            inputEmail?.requestFocus()
+        } else if (password.isEmpty() || password.length < 6) {
+            inputPassword?.error = "Enter proper password"
+            inputPassword?.requestFocus()
+        } else {
+            progressDialog?.setMessage("Please wait while login...")
+            progressDialog?.setTitle("Login")
+            progressDialog?.setCanceledOnTouchOutside(false)
+            progressDialog?.show()
+            loginUser(User(email, password))
+        }
+    }
+
+    private fun loginUser(user: User) {
+        UserModel.instance().loginUser(user) { task ->
+            if (task.isSuccessful()) {
+                progressDialog?.dismiss()
+                sendUserToNextActivity()
+                Toast.makeText(this@MainActivity, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                progressDialog?.dismiss()
+                Toast.makeText(this@MainActivity, "" + task.getException(), Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun sendUserToNextActivity() {
+        val intent: Intent = Intent(this@MainActivity, HomeActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 }
--- cocktailsPal/app/src/main/java/com/example/cocktailspal/RegisterActivity.kt
+++ cocktailsPal/app/src/main/java/com/example/cocktailspal/RegisterActivity.kt
@@ -8,7 +8,10 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cocktailspal.model.user.User
+import com.example.cocktailspal.model.user.UserModel
 
 class RegisterActivity : AppCompatActivity() {
     var alreadyHaveAccount: TextView? = null
@@ -16,6 +19,7 @@ class RegisterActivity : AppCompatActivity() {
     var inputPassword: EditText? = null
     var inputConfirmPassword: EditText? = null
     var btnRegister: Button? = null
+    var emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
     var progressDialog: ProgressDialog? = null
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,5 +42,49 @@ class RegisterActivity : AppCompatActivity() {
                 )
             )
         }
+        btnRegister!!.setOnClickListener { view: View? -> PerformAuth() }
+    }
+
+    private fun PerformAuth() {
+        val email: String = inputEmail?.text.toString()
+        val password: String = inputPassword?.text.toString()
+        val confirmPassword: String = inputConfirmPassword?.text.toString()
+        if (!email.matches(emailPattern.toRegex())) {
+            inputEmail?.error = "Enter correct email"
+            inputEmail?.requestFocus()
+        } else if (password.isEmpty() || password.length < 6) {
+            inputPassword?.error = "Enter proper password"
+            inputPassword?.requestFocus()
+        } else if (password != confirmPassword) {
+            inputConfirmPassword?.error = "Password not match both fields"
+            inputConfirmPassword?.requestFocus()
+        } else {
+            progressDialog?.setMessage("Please wait while registration...")
+            progressDialog?.setTitle("Registration")
+            progressDialog?.setCanceledOnTouchOutside(false)
+            progressDialog?.show()
+            registerUser(User(email, password))
+        }
+    }
+
+    private fun registerUser(user: User) {
+        UserModel.instance().registerUser(user) { task ->
+            if (task.isSuccessful()) {
+                progressDialog?.dismiss()
+                sendUserToNextActivity()
+                Toast.makeText(this@RegisterActivity, "Registration Successful", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                progressDialog?.dismiss()
+                Toast.makeText(this@RegisterActivity, "" + task.getException(), Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun sendUserToNextActivity() {
+        val intent: Intent = Intent(this@RegisterActivity, HomeActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 }
